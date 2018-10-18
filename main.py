04c31bee84c3f3180db1a6a7c9ab2646be14780c
Lukasz <kolodzinski.lukasz@gmail.com>
2018-10-18 14:43:29 +0200

    Initialized class ShoppingList

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ Project started: 18-10-2018
 Developed by Lukasz Kolodzinski
 """
 
-from tkinter import Tk as tk
+import tkinter as tk
 
+class ShoppingList(tk.Tk):
+    def __init__(self, products = None):
+        super().__init__()
+
+        if products is None:
+            self.products = []
+        else:
+            self.products = products
+
+shopping_list = ShoppingList()
+shopping_list.mainloop()
