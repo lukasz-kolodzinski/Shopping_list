cb56a61632af61bb94fe7b818a3b57f437db0959
Lukasz <kolodzinski.lukasz@gmail.com>
2018-11-05 11:44:08 +0100

    Created: add_player method

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ class TransferList(tk.Tk):
         self.create_wish.focus_set()
         self.bind("<Return>", self.add_player)
 
+    def add_player(self, event=None):
+        player_name = self.create_wish.get(1.0, tk.END).strip()
+        if len(player_name) > 0:
+            wished_player = tk.Label(self, text = player_name, pady=10)
+            wished_player.pack(side=tk.TOP, fill = tk.X)
+            self.players.append(wished_player)
+        self.create_wish.delete(1.0, tk.END)
+
 if __name__ == "__main__":
     transfer_list = TransferList()
     transfer_list.mainloop()
