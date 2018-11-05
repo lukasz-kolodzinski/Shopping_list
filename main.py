c31bc62985e773390eb8282faedcb62e08f629ea
Lukasz <kolodzinski.lukasz@gmail.com>
2018-11-05 14:28:24 +0100

    Added: color_schemes and colored players bars

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class TransferList(tk.Tk):
         self.title("Wish Transfer")
         self.geometry("500x700")
 
-        requested_player = tk.Label(self, text = "Add player's name here", bg = "gray", fg = "blue")
+        requested_player = tk.Label(self, text = "Add player's name here", bg = "lightgreen", fg = "black")
         self.players.append(requested_player)
 
         for player in self.players:
@@ -26,12 +26,17 @@ class TransferList(tk.Tk):
         self.create_wish.pack(side=tk.BOTTOM, fill=tk.X)
         self.create_wish.focus_set()
         self.bind("<Return>", self.add_player)
+        self.color_schemes = [{"bg": "lightgrey", "fg": "black"}, {"bg": "lightblue", "fg": "black"}]
 
     def add_player(self, event=None):
         player_name = self.create_wish.get(1.0, tk.END).strip()
         if len(player_name) > 0:
             wished_player = tk.Label(self, text = player_name, pady=10)
-            wished_player.pack(side=tk.TOP, fill = tk.X)
+            _, bar_style_choice = divmod(len(self.players), 2)
+            color_scheme_choose = self.color_schemes[bar_style_choice]
+            wished_player.configure(bg=color_scheme_choose["bg"])
+            wished_player.configure(fg=color_scheme_choose["fg"])
+            wished_player.pack(side=tk.TOP, fill=tk.X)
             self.players.append(wished_player)
         self.create_wish.delete(1.0, tk.END)
 
