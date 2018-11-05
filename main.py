66e312489393395b22b0ca3ec6118fd6e51d9617
Lukasz <kolodzinski.lukasz@gmail.com>
2018-11-05 10:16:32 +0100

    Code refactor

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 """
-Project started: 18-10-2018
 Developed by Lukasz Kolodzinski
 """
 
 import tkinter as tk
 
-class ShoppingList(tk.Tk):
-    def __init__(self, products = None):
+class TransferList(tk.Tk):
+    def __init__(self, players = None):
         super().__init__()
 
-        if products is None:
-            self.products = []
+        if players is None:
+            self.players = []
         else:
-            self.products = products
+            self.players = players
 
-        self.title("Shopping List")
+        self.title("Wish Transfer")
         self.geometry("500x700")
 
-shopping_list = ShoppingList()
-shopping_list.mainloop()
+        requested_player = tk.Label(self, text = "Add player's name here", bg = "gray", fg = "blue")
+        self.players.append(requested_player)
+
+        for player in self.players:
+            player.pack(side=tk.TOP, fill=tk.X)
+
+        self.create_wish = tk.Text(self, height=2, bg = "white", fg="black")
+        self.create_wish.pack(side=tk.BOTTOM, fill=tk.X)
+        self.create_wish.focus_set()
+        self.bind("<Return>", self.add_player)
+
+if __name__ == "__main__":
+    transfer_list = TransferList()
+    transfer_list.mainloop()
