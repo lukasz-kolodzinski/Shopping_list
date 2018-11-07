06867083fd380fc6fab51fa9a76bee37627364ac
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-11-07 22:33:19 +0100

    Fixed bugs related with frames and canvas

--- main.py
+++ main.py
@@ -16,29 +16,39 @@ class TransferList(tk.Tk):
         self.app_canvas = tk.Canvas(self)
         self.names_bar_frame = tk.Frame(self.app_canvas)
         self.text_frame = tk.Frame(self)
+
         self.scrolling = tk.Scrollbar(self.app_canvas, orient="vertical", command=self.app_canvas.yview)
         self.app_canvas.configure(yscrollcommand=self.scrolling.set)
+
         self.title("Wish Transfer")
         self.geometry("500x700")
-        self.create_wish = tk.Text(self, height=2, bg = "white", fg="black")
+
+        self.create_wish = tk.Text(self.text_frame, height=2, bg = "white", fg="black")
+
         self.app_canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.scrolling.pack(side=tk.RIGHT, fill=tk.Y)
-        self.canvas_frame = self.app_canvas.create_window((0, 0), window=self.names_bar_frame, anchor="n")
-        requested_player = tk.Label(self, text = "Add player's name here", bg = "lightgreen", fg = "black")
+
+        self.canvas_frame = self.app_canvas.create_window((0, 0), window=self.names_bar_frame, anchor="nw")
+        self.create_wish.pack(side=tk.BOTTOM, fill=tk.X)
+        self.text_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.create_wish.focus_set()
+
+        requested_player = tk.Label(self.names_bar_frame, text = "Add player's name here", bg = "lightgreen", fg = "black",
+                                    pady=10, padx=190)
+###        requested_player("<Button-1>", self.remove_task)
         self.players.append(requested_player)
 
         for player in self.players:
             player.pack(side=tk.TOP, fill=tk.X)
 
-        self.create_wish.pack(side=tk.BOTTOM, fill=tk.X)
-        self.create_wish.focus_set()
         self.bind("<Return>", self.add_player)
         self.color_schemes = [{"bg": "lightgrey", "fg": "black"}, {"bg": "lightblue", "fg": "black"}]
 
     def add_player(self, event=None):
         player_name = self.create_wish.get(1.0, tk.END).strip()
+
         if len(player_name) > 0:
-            wished_player = tk.Label(self, text = player_name, pady=10)
+            wished_player = tk.Label(self.names_bar_frame, text = player_name, pady=10)
             _, bar_style_choice = divmod(len(self.players), 2)
             color_scheme_choose = self.color_schemes[bar_style_choice]
             wished_player.configure(bg=color_scheme_choose["bg"])
